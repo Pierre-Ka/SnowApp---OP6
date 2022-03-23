ab7b6e7dbf36ab6ba2ad008c71f1aad4d70fe6b2
Pierre Ka <ikanhiu@outlook.fr>
2022-03-23 02:21:57 +0100

    FIX #28

---
+++ src/Entity/Video.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VideoRepository::class)]
+class Video
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: Trick::class, inversedBy: 'videos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $trick;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $path;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
