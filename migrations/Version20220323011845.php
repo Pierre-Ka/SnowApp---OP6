97183b2818424d6105a8e52c2ffeefb1ba5f0f29
Pierre Ka <ikanhiu@outlook.fr>
2022-03-24 05:54:42 +0100

    FIX #40

--- migrations/Version20220323011845.php
+++ migrations/Version20220323011845.php
@@ -14,7 +14,7 @@ final class Version20220323011845 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add Picture and Video table';
     }
 
     public function up(Schema $schema): void
