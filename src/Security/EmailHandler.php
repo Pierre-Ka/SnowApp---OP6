1a82120a86ae50a63ca37298d6fc35b235df8a68
Pierre Ka <ikanhiu@outlook.fr>
2022-03-26 13:01:03 +0100

    FIX #50

--- src/Security/EmailHandler.php
+++ src/Security/EmailHandler.php
@@ -4,47 +4,58 @@ namespace App\Security;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-/*  use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface; */
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class EmailHandler
 {
-     /* private $verifyEmailHelper; */
-    private $mailer;
-    private $entityManager;
+    private UrlGeneratorInterface $router;
+    private MailerInterface $mailer;
+    private EntityManagerInterface $em;
+    private TokenGeneratorInterface $tokenGenerator;
 
-    public function __construct(/* VerifyEmailHelperInterface $helper, */ MailerInterface $mailer, EntityManagerInterface $manager)
+    public function __construct(UrlGeneratorInterface $router, MailerInterface $mailer, EntityManagerInterface $manager, TokenGeneratorInterface $generator)
     {
-       /* $this->verifyEmailHelper = $helper; */
         $this->mailer = $mailer;
-        $this->entityManager = $manager;
+        $this->router = $router;
+        $this->em = $manager;
+        $this->tokenGenerator = $generator;
     }
 
-    public function sendEmailConfirmation(string $verifyEmailRouteName, UserInterface $user, TemplatedEmail $email): void
+    public function sendConfirmationMail(UserInterface $user)
     {
-
-        $signatureComponents = $this->verifyEmailHelper->generateSignature(
-            $verifyEmailRouteName,
-            $user->getId(),
-            $user->getEmail()
-        );
-        $context = $email->getContext();
-        $context['signedUrl'] = $signatureComponents->getSignedUrl();
-        $context['expiresAtMessageKey'] = $signatureComponents->getExpirationMessageKey();
-        $context['expiresAtMessageData'] = $signatureComponents->getExpirationMessageData();
-        $email->context($context);
+        $link = $this->generateUrlForEmailConfirmation('app_verify_email', $user);
+        $email = (new TemplatedEmail())
+            ->from('SnowTrick@noreply.com')
+            ->to($user->getEmail())
+            ->subject('Confirmer votre email')
+            ->htmlTemplate('emails/signup.html.twig')
+            ->context([
+                'link' => $link,
+                'user' => $user,
+                    ] );
         $this->mailer->send($email);
     }
 
-    public function handleEmailConfirmation(Request $request, UserInterface $user): void
+    public function generateUrlForEmailConfirmation(string $routeName, UserInterface $user, array $extraParams = []): string
     {
-        $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
+        $token = $this->tokenGenerator->generateToken();
+        $user->setToken($token);
+        $this->em->persist($user);
+        $this->em->flush();
+        $extraParams['token'] = $token;
+        $extraParams['email'] = $user->getEmail();
+        $uri = $this->router->generate($routeName, $extraParams, UrlGeneratorInterface::ABSOLUTE_URL);
+        return $uri;
+    }
 
+    public function verifyUser(UserInterface $user): void
+    {
         $user->setIsVerified(true);
-
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        $user->eraseCredentials();
+        $this->em->persist($user);
+        $this->em->flush();
     }
 }
