932e3a88ab7ba12fb53962ce9986e29ad8bbc640
Pierre Ka <ikanhiu@outlook.fr>
2022-04-21 17:53:12 +0200

    FIX #94

--- assets/js/collection_on_trick_create.js
+++ assets/js/collection_on_trick_create.js
@@ -1,8 +1,8 @@
 /* *********************************** COLLECTION ON TRICK CREATE ***************************************** */
 
-const VideosCollectionHolder = document.querySelector("#global_prototype_videos");
-let indexVideos = VideosCollectionHolder.querySelectorAll("fieldset").length;
+let indexVideos = document.querySelector("#global_prototype_videos").querySelectorAll("fieldset").length;
 document.querySelector("#new_video").addEventListener("click", function() {
+    const VideosCollectionHolder = document.querySelector("#global_prototype_videos");
     VideosCollectionHolder.innerHTML += VideosCollectionHolder.dataset.prototype.replace(/__name__/g, indexVideos);
     indexVideos ++;
 });
@@ -10,6 +10,5 @@ const PicturesCollectionHolder = document.querySelector("#global_prototype_pictu
 let indexPictures = PicturesCollectionHolder.querySelectorAll("fieldset").length;
 document.querySelector("#new_picture").addEventListener("click", function() {
     PicturesCollectionHolder.innerHTML += PicturesCollectionHolder.dataset.prototype.replace(/__name__/g, indexPictures);
-    console.log('la');
     indexPictures ++;
 });
