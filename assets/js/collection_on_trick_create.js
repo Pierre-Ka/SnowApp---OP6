02be8efe1c4b5fc775821c54c3416b1d081d8f82
Pierre Ka <ikanhiu@outlook.fr>
2022-04-22 14:55:41 +0200

    FIX #96

--- assets/js/collection_on_trick_create.js
+++ assets/js/collection_on_trick_create.js
@@ -1,14 +1,36 @@
 /* *********************************** COLLECTION ON TRICK CREATE ***************************************** */
 
-let indexVideos = document.querySelector("#global_prototype_videos").querySelectorAll("fieldset").length;
+async function removeItem(e) {
+    e.currentTarget.closest(".removal").remove();
+}
+
+const VideosCollectionHolder = document.querySelector("#space_where_prototype_videos_is_added");
+let indexVideos = VideosCollectionHolder.querySelectorAll("fieldset").length;
 document.querySelector("#new_video").addEventListener("click", function() {
-    const VideosCollectionHolder = document.querySelector("#global_prototype_videos");
-    VideosCollectionHolder.innerHTML += VideosCollectionHolder.dataset.prototype.replace(/__name__/g, indexVideos);
+    const item = document.createElement("div");
+    item.classList.add("removal");
+    item.innerHTML = VideosCollectionHolder.dataset.prototype.replace(/__name__/g, indexVideos);
     indexVideos ++;
+    item.querySelector(".btn-remove").addEventListener("click", function(e) {
+        removeItem(e);
+    });
+    VideosCollectionHolder.appendChild(item);
 });
-const PicturesCollectionHolder = document.querySelector("#global_prototype_pictures");
+const PicturesCollectionHolder = document.querySelector("#space_where_prototype_pictures_is_added");
 let indexPictures = PicturesCollectionHolder.querySelectorAll("fieldset").length;
 document.querySelector("#new_picture").addEventListener("click", function() {
-    PicturesCollectionHolder.innerHTML += PicturesCollectionHolder.dataset.prototype.replace(/__name__/g, indexPictures);
+    const item = document.createElement("div");
+    item.classList.add("removal");
+    item.innerHTML = PicturesCollectionHolder.dataset.prototype.replace(/__name__/g, indexPictures);
     indexPictures ++;
+    item.querySelector(".btn-remove").addEventListener("click", function(e) {
+        removeItem(e);
+    });
+    PicturesCollectionHolder.appendChild(item);
+});
+
+document.querySelectorAll(".btn-remove").forEach(eachOne => {
+    eachOne.addEventListener('click', function(e) {
+        removeItem(e);
+    });
 });
